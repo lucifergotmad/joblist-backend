93201f1affcb9ae430d81edd367ae355aeb7ce77
L u c i f e r <amateur.fullstackdev@gmail.com>
2021-04-01 23:11:53 +0700

    adding seeder script

---
+++ seeder.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import users from "./data/users.js";
+import User from "./models/userModel";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+  try {
+    await User.deleteMany();
+
+    await User.insertMany(users);
+    console.log("Data Imported");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await User.deleteMany();
+
+    console.log("Data Destroyed");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-d") {
+  destroyData();
+} else {
+  importData();
+}
